660df289f803810ae8645ec7f4965c1aa51f62b5
Josh Wilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2024-02-12 11:06:34 +0800

    audit role stackset

---
+++ modules/aws-idc-integration/audit-roles/main.tf
@@ -0,0 +1,35 @@
+locals {
+  role_name = "${var.namespace}-${var.stage}-audit-role"
+}
+
+resource "aws_cloudformation_stack_set" "audit_roles" {
+  name = "${var.namespace}-${var.stage}-audit-role-stack"
+  auto_deployment {
+    enabled                          = true
+    retain_stacks_on_account_removal = false
+  }
+
+  permission_model = "SERVICE_MANAGED"
+  capabilities     = ["CAPABILITY_IAM", "CAPABILITY_NAMED_IAM"]
+  parameters = {
+    ExternalID        = var.external_id
+    CommonFateAccount = var.common_fate_aws_account_id
+    RoleName          = local.role_name
+  }
+
+  template_body = file("${path.module}/audit-role.yaml")
+  lifecycle {
+    ignore_changes = [parameters.RoleName]
+  }
+}
+
+
+resource "aws_cloudformation_stack_set_instance" "audit_role_stackset_instance" {
+  for_each = toset(var.regions)
+  deployment_targets {
+    organizational_unit_ids = var.organizational_unit_ids
+  }
+  region         = each.key
+  stack_set_name = aws_cloudformation_stack_set.audit_roles.name
+}
+
