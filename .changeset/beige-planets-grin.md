5bdb6695bea5fe8ae1c92a5f7bc5ee7d80dcb4c2
Shwetha Umashanker <32020525+shwethaumashanker@users.noreply.github.com>
2024-02-28 13:17:43 -0500

    provisioner_task_role_name (#85)

--- .changeset/beige-planets-grin.md
+++
@@ -1,5 +0,0 @@
----
-"@common-fate/terraform-aws-common-fate-deployment": minor
----
-
-Allows RDS deletion protection to be disabled by setting the `database_deletion_protection` to `false`. Deletion protection is enabled by default, but can now be disabled in order to destroy the stack.
--- modules/provisioner/outputs.tf
+++ modules/provisioner/outputs.tf
@@ -12,7 +12,7 @@ output "task_role_arn" {
 }
 
 output "task_role_name" {
-  description = "The ARN of the IAM role assumed by the task"
-  value       = aws_iam_role.provisioner_ecs_task_role.arn
+  description = "The name of the IAM role assumed by the task"
+  value       = aws_iam_role.provisioner_ecs_task_role.name
 }
 
