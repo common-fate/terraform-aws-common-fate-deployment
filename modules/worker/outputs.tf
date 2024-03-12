1b3af646fb9305df2bd4633e121c427ea4578b4f
JoshuaWilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2024-03-12 12:00:04 +0800

    fix outputs

--- modules/worker/outputs.tf
+++ modules/worker/outputs.tf
@@ -4,10 +4,10 @@
 
 output "task_role_arn" {
   description = "The ARN of the IAM role assumed by the task"
-  value       = aws_iam_role.control_plane_ecs_task_role.arn
+  value       = aws_iam_role.worker_ecs_task_role.arn
 }
 
 
 output "security_group_id" {
-  value = aws_security_group.ecs_control_plane_sg_v2.id
+  value = aws_security_group.ecs_worker_sg.id
 }
