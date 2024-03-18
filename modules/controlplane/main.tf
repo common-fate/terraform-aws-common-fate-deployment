4cdba9a8122e8b971cfa536776d7dbe1f2439920
chrnorm <17420369+chrnorm@users.noreply.github.com>
2024-03-18 18:58:23 +0000

    revert path routing changes

--- modules/controlplane/main.tf
+++ modules/controlplane/main.tf
@@ -723,12 +723,7 @@ resource "aws_lb_listener_rule" "service_rule" {
   }
   condition {
     path_pattern {
-      values = [
-        "/commonfate.control*",
-        "/api/*",
-        "/commonfate.leastprivilege*",
-        "/commonfate.authz.v1alpha1.EvaluationService/*" // authz evaluation service is handled by Control Plane.
-      ]
+      values = ["/commonfate.control*", "/api/*", "/commonfate.leastprivilege*"]
     }
   }
 }
