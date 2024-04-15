cde68345b03810db314d20fe4911ebae8bcf0697
chrnorm <17420369+chrnorm@users.noreply.github.com>
2024-04-15 13:33:56 +0100

    fix contains() validation

--- modules/alerts/variables.tf
+++ modules/alerts/variables.tf
@@ -18,7 +18,7 @@ variable "alerts" {
 
   validation {
     condition = alltrue([
-      for o in var.alerts : contains("errors", "all", o.value)
+      for o in var.alerts : contains(["errors", "all"], o.value)
     ])
     error_message = "the alert level must be either 'errors' or 'all'"
   }
