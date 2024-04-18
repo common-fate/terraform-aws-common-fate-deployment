0321530c57f417c857e2e3a23619534f483d6ec9
meyerjrr <33018450+meyerjrr@users.noreply.github.com>
2024-04-18 16:57:37 +1000

    add alb rule to allow new granted service (#165)
    * add alb rule to allow new granted service
    
    * changeset
    
    * Update .changeset/metal-moles-sin.md
    
    ---------
    
    Co-authored-by: Chris Norman <17420369+chrnorm@users.noreply.github.com>

---
+++ .changeset/metal-moles-sin.md
@@ -0,0 +1,5 @@
+---
+"@common-fate/terraform-aws-common-fate-deployment": patch
+---
+
+For BYOC customers: the Common Fate Control plane now serves the Granted Profile Registry API. We've updated the load balancer rules to reflect this.
--- modules/controlplane/main.tf
+++ modules/controlplane/main.tf
@@ -788,7 +788,7 @@ resource "aws_lb_listener_rule" "service_rule" {
   }
   condition {
     path_pattern {
-      values = ["/commonfate.control*", "/api/*", "/commonfate.leastprivilege*"
+      values = ["/commonfate.control*", "/api/*", "/commonfate.leastprivilege*", "/granted*"
       ]
     }
   }
