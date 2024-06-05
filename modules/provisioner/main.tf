7c99c37de95b366239a59cc1844005e95be548d7
JoshuaWilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2024-06-05 08:13:05 +0800

    Update modules/provisioner/main.tf
    Co-authored-by: Calvin <70191007+ckluy31@users.noreply.github.com>

--- modules/provisioner/main.tf
+++ modules/provisioner/main.tf
@@ -11,7 +11,6 @@ locals {
   name_prefix = join("-", compact([var.namespace, var.stage, var.name_prefix]))
 }
 
-# Use 'local.combined_env_vars' wherever you need to pass these environment variables
 #trivy:ignore:AVD-AWS-0104
 resource "aws_security_group" "ecs_provisioner_sg_v2" {
   name        = "${local.name_prefix}-provisioner"
