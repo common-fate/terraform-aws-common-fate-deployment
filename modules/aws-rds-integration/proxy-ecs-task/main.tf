6d99969f6a86fbe8082cbc3c6489f54e26680fa7
JoshuaWilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2024-06-19 14:50:54 +0800

    fix duplicate resource name

--- modules/aws-rds-integration/proxy-ecs-task/main.tf
+++ modules/aws-rds-integration/proxy-ecs-task/main.tf
@@ -132,7 +132,7 @@ resource "aws_iam_role_policy_attachment" "assume_roles_policy_attach_tagged" {
   policy_arn = aws_iam_policy.assume_rds_proxy_role_tagged.arn
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_ssm" {
+resource "aws_iam_role_policy_attachment" "ecs_task_role_ssm" {
   role       = aws_iam_role.rds_proxy_ecs_task_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
