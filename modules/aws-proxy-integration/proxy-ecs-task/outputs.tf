9443e389cc3aa2266fdc5524606aefcc45dca981
JoshuaWilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2024-07-08 13:07:08 +0800

    fix outputs

--- modules/aws-proxy-integration/proxy-ecs-task/outputs.tf
+++ modules/aws-proxy-integration/proxy-ecs-task/outputs.tf
@@ -5,12 +5,12 @@
 
 output "task_role_arn" {
   description = "The ARN of the IAM role assumed by the task"
-  value       = aws_iam_role.rds_proxy_ecs_task_role.arn
+  value       = aws_iam_role.proxy_ecs_task_role.arn
 }
 
 output "task_role_name" {
   description = "The name of the IAM role assumed by the task"
-  value       = aws_iam_role.rds_proxy_ecs_task_role.name
+  value       = aws_iam_role.proxy_ecs_task_role.name
 }
 
 output "security_group_id" {
