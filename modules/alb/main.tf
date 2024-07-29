435d8c9fac7451944e93a5d05eb8832cf8ba50a1
ckluy31 <calvinluy@gmail.com>
2024-07-30 09:40:58 +1000

    removed unneeded lb target group

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -105,11 +105,3 @@ resource "aws_lb_listener_certificate" "additional_certs" {
   listener_arn    = aws_lb_listener.https_listener.arn
   certificate_arn = each.value
 }
-
-resource "aws_lb_target_group" "main" {
-  name        = "${var.namespace}-${var.stage}-tg"
-  port        = 80
-  protocol    = "HTTP"
-  vpc_id      = var.vpc_id
-  target_type = "instance"
-}
