73e84c3c5054f3f763d946dad0d35e3a18cc393a
JoshuaWilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2024-08-08 14:48:34 +0800

    Add an output for the load balancer ARN (#294)

--- modules/alb/outputs.tf
+++ modules/alb/outputs.tf
@@ -14,5 +14,9 @@ output "listener_arn" {
 
 output "alb_security_group_id" {
   description = "the id for the security group managing the alb"
-  value = aws_security_group.alb_sg.id
+  value       = aws_security_group.alb_sg.id
+}
+output "load_balancer_arn" {
+  description = "ARN of the load balancer."
+  value       = aws_lb.main_alb.arn
 }
