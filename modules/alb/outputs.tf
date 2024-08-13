71f466fa06a46f06cbbf8b178c21d5b0ef26de3c
JoshuaWilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2024-08-13 12:27:21 +0800

    use listener ad dependency for LB arn

--- modules/alb/outputs.tf
+++ modules/alb/outputs.tf
@@ -14,14 +14,16 @@ output "listener_arn" {
 
 output "alb_security_group_id" {
   description = "the id for the security group managing the alb"
-  value = aws_security_group.alb_sg.id
+  value       = aws_security_group.alb_sg.id
 }
 output "load_balancer_arn" {
   description = "ARN of the load balancer."
-  value       = aws_lb.main_alb.arn
+  // make downstream modules depend on the https listener being deployed before attempting to use this ARN
+  // For example, this means the ECS tasks will need to wait for the listerner being attached before creating target groups
+  value = aws_lb_listener.https_listener.load_balancer_arn
 }
 
 output "alb_arn_suffix" {
   description = "The arn suffix of the load balancer"
-  value = aws_lb.main_alb.arn_suffix
+  value       = aws_lb.main_alb.arn_suffix
 }
