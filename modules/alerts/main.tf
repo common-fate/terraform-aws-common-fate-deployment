8a59c605ce45e198698cb34334ce949355a22f83
Shwetha Umashanker <32020525+shwethaumashanker@users.noreply.github.com>
2024-08-21 16:45:42 -0400

    removed count

--- modules/alerts/main.tf
+++ modules/alerts/main.tf
@@ -39,9 +39,6 @@ resource "aws_cloudwatch_event_rule" "deployment_failures" {
 }
 
 resource "aws_cloudwatch_event_target" "deployment_failures" {
-  # Only define aws_cloudwatch_event_target when use_opsgenie_format is false
-  count = var.use_opsgenie_format ? 0 : 1
-
   rule      = aws_cloudwatch_event_rule.deployment_failures[0].name
   target_id = "${var.namespace}-${var.stage}-deployment-failures-to-sns"
   arn       = aws_sns_topic.deployment_failures.arn
@@ -115,8 +112,6 @@ resource "aws_cloudwatch_event_rule" "job_failures" {
 
 
 resource "aws_cloudwatch_event_target" "job_failures" {
-  # Only define aws_cloudwatch_event_target when use_opsgenie_format is false
-  count = var.use_opsgenie_format ? 0 : 1
 
   rule           = aws_cloudwatch_event_rule.job_failures[0].name
   target_id      = "${var.namespace}-${var.stage}-job-failures"
