661c6e2696442ca8fef53972e47c55444eaa5597
chrnorm <17420369+chrnorm@users.noreply.github.com>
2024-09-05 15:08:40 +0100

    replace the hardcoded provider block

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
-provider "aws" {
-  region = var.aws_region
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
 
 data "aws_partition" "current" {}
